96e132267c8c5a1ea8ec03f28a8e72853429ab11
Steven Weaver <sweaver@temple.edu>
2024-12-27 14:12:33 -0600

    Commit Message:
    "Add multi-language 'Hello, World!' program implementations in Python, Java, JavaScript, C, and C++."

---
+++ generated_code.py
@@ -0,0 +1,30 @@
+```python
+# Python implementation
+print("Hello, World!")
+
+# Java implementation
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+# JavaScript implementation
+console.log("Hello, World!");
+
+# C implementation
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+# C++ implementation
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+```
